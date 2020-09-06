4526646855382bffbbba4e675e3fd5a359935679
Amit kumar goswami <cseiubatgoswami@gmail.com>
2020-09-07 05:08:23 +0600

    first commit

---
+++ cat generator/static/JS/script.js
@@ -0,0 +1,6 @@
+function ganarateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = 'static/images/download.jpg';
+    div.appendChild(image);
+}
